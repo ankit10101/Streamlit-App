63901ba8d8e107428e55508553a103c72afd523c
Ankit Bajaj <40112826+ankit10101@users.noreply.github.com>
2025-10-14 17:44:32 +0530

    Updated Agent ID

--- app.py
+++ app.py
@@ -16,16 +16,20 @@ with st.sidebar:
     st.write(
         "*Analytics Made Simple — One Question at a Time. Powered by AWS Bedrock AgentCore*"
     )
+
     st.caption(
         """**  ADAM (AI-Driven Analytics Manager) — Your intelligent partner for automating the entire digital analytics lifecycle 
                with mastery in GTM, GA4, and Selenium-powered browser automation.**
         """
     )
+
     st.divider()
+
     # ✅ Clear Chat Button
     if st.button("🧹 Clear Chat"):
         st.session_state.messages = []
         st.rerun()
+
     st.caption(
         "<p style = 'text-align:center'>Made with ❤️</p>",
         unsafe_allow_html=True,
@@ -53,7 +57,7 @@ if user_input := st.chat_input("How may I help you?"):
             payload = json.dumps({"prompt": user_input})
 
             response = client.invoke_agent_runtime(
-                agentRuntimeArn="arn:aws:bedrock-agentcore:ap-south-1:501931553097:runtime/ADAM-PLkNRwHAth",
+                agentRuntimeArn="arn:aws:bedrock-agentcore:ap-south-1:501931553097:runtime/hosted_agent_mb5wa-JU4BeUCksB",
                 runtimeSessionId="dfmeoagmreaklgmrkleafremoigrmtesogmtrskhmtkrlshmt",  # Must be 33+ chars
                 payload=payload,
                 qualifier="DEFAULT",  # Optional
@@ -63,6 +67,7 @@ if user_input := st.chat_input("How may I help you?"):
             response_data = json.loads(response_body)["result"]
 
             st.markdown(response_data)
+
         st.session_state.messages.append(
             {"role": "assistant", "content": response_data}
         )
